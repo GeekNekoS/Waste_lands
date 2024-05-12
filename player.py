852a5b4364172d7656d8af7cf036a41b6c92f98c
GeekNekoS <anastasiyanesterenko@icloud.com>
2024-05-13 03:36:48 +0500

    The sounds of the player's footsteps are implemented

--- player.py
+++ player.py
@@ -1,10 +1,55 @@
 import pygame
 from config import player_sprites
 from settings import debug, WIDTH, HEIGHT
+import random
+
+
+pygame.mixer.init()
+footstep_sounds = [
+    pygame.mixer.Sound('sounds/footsteps/footstep_1.wav'),
+    pygame.mixer.Sound('sounds/footsteps/footstep_2.wav'),
+    pygame.mixer.Sound('sounds/footsteps/footstep_3.wav')
+]
+
+
+class PlayerSounds:
+    def __init__(self, footstep_speed=0.3):
+        # Загрузка звуковых файлов
+        self.footstep_sounds = footstep_sounds
+
+        # Скорость воспроизведения звуков ходьбы
+        self.footstep_speed = footstep_speed
+        self.footstep_timer = 0  # Таймер для отслеживания времени между звуками
+        self.next_footstep_time = 0  # Время до следующего звука
+
+        self.set_footstep_volume(0.3)  # Начальная громкость звуков шагов
+
+    def update(self, dt):
+        # Обновляем таймер
+        self.footstep_timer += dt
+
+    def play_footstep(self):
+        # print("Playing footstep")
+        # Проверяем, прошло ли достаточно времени для воспроизведения следующего звука
+        if self.footstep_timer >= self.next_footstep_time:
+            # Воспроизводим случайный звук шага
+            footstep_sound = random.choice(self.footstep_sounds)
+            footstep_sound.play()
+
+            # Сбрасываем таймер
+            self.footstep_timer = 0
+
+            # Устанавливаем время до следующего звука
+            self.next_footstep_time = self.footstep_speed
+
+    def set_footstep_volume(self, volume):
+        """Устанавливает громкость звуков шагов."""
+        for sound in self.footstep_sounds:
+            sound.set_volume(volume)
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, start_pos):
+    def __init__(self, start_pos, player_sounds):
         super().__init__()
         # Загрузка спрайтов для всех направлений
         self.sprites = {
@@ -32,13 +77,15 @@ class Player(pygame.sprite.Sprite):
             hitbox_height
         )
 
+        self.player_sounds = player_sounds  # Сохраняем экземпляр класса PlayerSounds
+        self.is_moving = False  # Инициализируем флаг движения
+
     def update(self, keys, dt, trees):
+        self.player_sounds.update(dt)  # Обновляем таймер звуков игрока
         self.move(keys, dt, trees)
         if self.is_moving:
             self.animate(dt)
-        else:
-            self.current_frame = 0
-            self.image = self.current_sprites[self.current_frame]
+            self.player_sounds.play_footstep()  # Воспроизводим звуки ходьбы
 
     def animate(self, dt):
         self.animation_counter += dt
